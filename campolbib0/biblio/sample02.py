1e091a653b10dc0b1b4daa438842f329ddc624fd
ubriquejazz <ubriquejazz@gmail.com>
2019-10-25 06:35:43 +0000

    2000 iterations

--- campolbib0/biblio/sample02.py
+++ campolbib0/biblio/sample02.py
@@ -8,9 +8,9 @@ from biblio.models import Book, Borrowing, LibraryUser, CATEGORY_CHOICES
 
 import pdb
 
-# INPUT_FILE_STR = '../Katalog_2019_10_20_1_4775.yaml'
+INPUT_FILE_STR = '../Katalog_2019_10_20_1_4775.yaml'
 # INPUT_FILE_STR = '../Katalog_simple.yaml'
-INPUT_FILE_STR = '../Katalog_2019_10_20_2_100.yaml'
+# INPUT_FILE_STR = '../Katalog_2019_10_20_2_100.yaml'
 ROWS_INI = 0
 ROWS_END = 20
 
@@ -43,68 +43,85 @@ def classify_status(status_str):
     else:
         return 'NONAVAILABLE'
 
-def main():
-    # pdb.set_trace()
-    global INPUT_FILE_STR, ROWS_INI, ROWS_END
+def read_yaml(input_file_str):
 
-    with open(INPUT_FILE_STR, 'r') as file_desc:
+    with open(input_file_str, 'r') as file_desc:
         yaml_data = yaml.load(file_desc, Loader=yaml.Loader)
 
-    yaml_entries = yaml_data[ROWS_INI:(ROWS_END+1)]
+    return yaml_data
+
+def main(yaml_data):
+    # pdb.set_trace()
+    # global INPUT_FILE_STR, ROWS_INI, ROWS_END
+
+    # with open(INPUT_FILE_STR, 'r') as file_desc:
+    #     yaml_data = yaml.load(file_desc, Loader=yaml.Loader)
+    # yaml_data = read_yaml(INPUT_FILE_STR)
+    # pdb.set_trace()
+
+    # yaml_entries = yaml_data[ROWS_INI:(ROWS_END+1)]
+
     # for yaml_book in yaml_entries:
-    for index in range(0, len(yaml_entries)):
-        yaml_book = yaml_entries[index]
+    # for index in range(0, len(yaml_entries)):
+    for index in range(0, len(yaml_data)):
+        # yaml_book = yaml_entries[index]
+        yaml_book = yaml_data[index]
 
-        # print("Iteration: %s" % (index+ROWS_INI))
-        print("Iteration: %d" % int(yaml_book['Number']))
-        title          = yaml_book['Title']
-        author_name    = yaml_book['Name']
-        author_surname = yaml_book['Surname']
-        publisher_name = yaml_book['Publisher']
-        publisher_city = yaml_book['City']
-        year_published = norm_int(yaml_book['Year'])
+        try:
+            # print("Iteration: %s" % (index+ROWS_INI))
+            print("Iteration: %d" % int(yaml_book['Number']))
+            title          = yaml_book['Title']
+            author_name    = yaml_book['Name']
+            author_surname = yaml_book['Surname']
+            publisher_name = yaml_book['Publisher']
+            publisher_city = yaml_book['City']
+            year_published = norm_int(yaml_book['Year'])
 
-        ISBN           = yaml_book['ISBN']
-        category       = classify_cat(yaml_book['Category'])
-        status         = classify_status(yaml_book['Available'])
-        location       = norm_str(yaml_book['Location'])
-        quantity       = norm_int(yaml_book['Amount'])
+            ISBN           = yaml_book['ISBN']
+            category       = classify_cat(yaml_book['Category'])
+            status         = classify_status(yaml_book['Available'])
+            location       = norm_str(yaml_book['Location'])
+            quantity       = norm_int(yaml_book['Amount'])
 
-        description    = yaml_book['Description']
-        notes          = yaml_book['Anotations']
+            description    = yaml_book['Description']
+            notes          = yaml_book['Anotations']
 
-        for item in range(1, quantity+1):
-            #'''
-            book = Book(
-                title=title,
-                author_name=author_name,
-                author_surname=author_surname,
-                publisher_name=publisher_name, 
-                publisher_city= publisher_city,
-                year_published=year_published,
-                #ISBN=ISBN,
-                category=category,
-                status=status,
-                location=location,
-                #description=description,
-                #notes=notes,
-                )
-            #'''
-            if ISBN is not None:
-                book.ISBN = ISBN
-            if description is not None:
-                book.description = description
-            if notes is not None:
-                book.notes = notes
+            for item in range(1, quantity+1):
+                #'''
+                book = Book(
+                    title=title,
+                    author_name=author_name,
+                    author_surname=author_surname,
+                    publisher_name=publisher_name, 
+                    publisher_city=publisher_city,
+                    year_published=year_published,
+                    #ISBN=ISBN,
+                    category=category,
+                    status=status,
+                    location=location,
+                    #description=description,
+                    #notes=notes,
+                    )
+                #'''
+                if ISBN is not None:
+                    book.ISBN = ISBN
+                if description is not None:
+                    book.description = description
+                if notes is not None:
+                    book.notes = notes
 
-            save = True
+                save = True
 
-            print_book(book)
+                print_book(book)
 
+                pdb.set_trace()
+
+                if save is True:
+                    book.save()
+
+        except Exception as e:
+            print(e)
             pdb.set_trace()
-
-            if save is True:
-                book.save()
 
 
 if __name__ == "__main__":
