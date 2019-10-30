34a99daecefdb82304258d4b51db3db64ae30d6a
ubriquejazz <ubriquejazz@gmail.com>
2019-10-30 23:18:08 +0000

    moved folders

--- campolbib0/biblio/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from biblio.models import Book, LibraryUser, Borrowing
-
-admin.site.register(Book)
-admin.site.register(LibraryUser)
-admin.site.register(Borrowing)
--- campolbib0/biblio/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BiblioConfig(AppConfig):
-    name = 'biblio'
